cf85f5830c3b4e50f9cbdeecce7669d1d5dde02b
Pablo <pabloalonsogarc@gmailc.om>
2022-04-19 21:01:32 +0200

    Metodo imprimr

--- src/main/java/metodos.java
+++ src/main/java/metodos.java
@@ -1,24 +1,38 @@
 import java.util.Scanner;
 public class metodos {
-    public static void pedir(int num[]){
+    public static void pedir(int num[]) {
         Scanner sc = new Scanner(System.in);
-        for(int cont=0;cont<10;cont++){
+        for (int cont = 0; cont < 10; cont++) {
             System.out.println("Número:");
-            num[cont]=Integer.parseInt(sc.nextLine());
+            num[cont] = Integer.parseInt(sc.nextLine());
 
         }
     }
-    public static void comparar(int num[],int num2[]){
-        int cont=0;
-        for(int cont2=0;cont2<10;cont2++){
-            cont=1;
-            for(int cont3=0;cont3<10;cont3++){
-                if(num[cont2] == num[cont3])
-                    num2[cont2]=cont++;
+
+    public static void comparar(int num[], int num2[]) {
+        int cont = 0;
+        for (int cont2 = 0; cont2 < 10; cont2++) {
+            cont = 1;
+            for (int cont3 = 0; cont3 < 10; cont3++) {
+                if (num[cont2] == num[cont3])
+                    num2[cont2] = cont++;
             }
         }
     }
-    public static void imprimir(int num[],int num2[]){
 
+    public static void imprimir(int num[], int num2[]) {
+        int var = 0;
+        System.out.printf("\n[");
+        for (int x = 0; x < 10; x++) {
+            if (num[x] != var) {
+                if (x == 9)
+                    System.out.printf("%d=%d", num[x], num2[x]);
+                else
+                    System.out.printf("%d=%d", num2[x], num[x]);
+            }
+            var = num[x];
+        }
+        System.out.printf("]");
     }
 }
+
